cd8318979c4ece4d44f8b1fc8072a360e4fdc189
Jean Leduc <jean.leduc80@gmail.com>
2023-02-03 16:45:44 +0100

    03 02 2023 fin de journée

--- connexion.php
+++ connexion.php
@@ -4,32 +4,64 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-
-<head>
-    <link rel="stylesheet" href="connexion.css">
+	<link rel="stylesheet" type="text/css" href="inscription.css">
+	<title>S'inscrire</title>
 </head>
 
 <body>
 
-<div class="login-form">
-		<form action="SiteFilm.php" method="post">
+<?php
+$ipserver ="192.168.65.164";
+$nomBase = "VoteFilm";
+$loginPrivilege = "root";
+$passPrivilege = "root";
+
+try{
+    $GLOBALS["pdo"] = new PDO('mysql:host=' . $ipserver . ';dbname=' . $nomBase . '', $loginPrivilege, $passPrivilege);
+    echo "début";
+}catch(Exception $e){
+    echo $e->getMessage();
+}
+
+
+
+?>
+
+	<div class="login-form">
+		<form action="" method="post">
 			<h2>Se connecter</h2>
 			<div class="form-group">
 				<label for="username">Nom d'utilisateur:</label>
-				<input type="text" placeholder="Pseudo" name="pseudo" id="username" required>
+				<input type="text" placeholder="Pseudo" name="pseudo">
 			</div>
 			<div class="form-group">
 				<label for="password">Mot de passe:</label>
-				<input type="password" placeholder="Password" name="password" id="password" required>
+				<input type="password" placeholder="Password" name="mdp">
 			</div>
-			<input type="submit" value="Se connecter">
-		</form>
-		<form action="inscription.php" target="_blank">
-					<input type="submit" value="Inscription">
+            <input type="submit" name="login" value="Se connecter"></input>
 				</form>
+                <form action="inscription.php" method="post">
+                <input type="submit" name="inscription" value="inscription"></input>
+</form>
 	</div>
+
+<?php
+
+$pseudo = $_POST['pseudo'];
+$mdp = $_POST['mdp'];
+
+$query = "SELECT * FROM User WHERE pseudo='$pseudo' AND mdp = '$mdp'";
+$reseult = mysql_query($conn, $query);
+$row = mysql_fetch_assoc($result);
+
+if(mysql_num_rows($result) > 0) {
+    $role = $row['role'];
+    $_SESSION["Login"] = $pseudo;
+    $_SESSION["mdp"] = $mdp;
+    $_SESSION["Isconnect"] = true;
+}
+
+ ?>
 
 </body>
 </html>
--- inscription.php
+++ inscription.php
@@ -10,8 +10,25 @@
 
 <body>
 
+<?php
+$ipserver ="192.168.65.164";
+$nomBase = "VoteFilm";
+$loginPrivilege = "root";
+$passPrivilege = "root";
+
+try{
+    $GLOBALS["pdo"] = new PDO('mysql:host=' . $ipserver . ';dbname=' . $nomBase . '', $loginPrivilege, $passPrivilege);
+    echo "début";
+}catch(Exception $e){
+    echo $e->getMessage();
+}
+
+
+
+?>
+
 	<div class="login-form">
-		<form action="" method="post">
+		<form action="connexion.php" method="post">
 			<h2>S'inscrire</h2>
 			<div class="form-group">
 				<label for="username">Nom d'utilisateur:</label>
@@ -21,17 +38,18 @@
 				<label for="password">Mot de passe:</label>
 				<input type="password" placeholder="Password" name="mdp">
 			</div>
-		</form>
-		<form action="connexion.php"" target="_blank">
-					<input type="submit" name="Inscription" value="inscription"></input>
+					<input type="submit" name="inscription" value="inscription"></input>
 				</form>
 	</div>
 
 <?php
-
 if(isset($_POST["inscription"])){
-    $requeteUser = "INSERT INTO 'User' ('pseudo', 'mdp') VALUES ('" .$_POST['pseudo'] . "','" .$_POST['mdp']."')";
+    echo "test".$_POST['pseudo'];
+    $requeteUser = "INSERT INTO User (pseudo, mdp) VALUES ('" .$_POST['pseudo']. "','" .$_POST['mdp']."')";
+    echo "requete : ".$requeteUser;
     $result2 = $GLOBALS["pdo"]->query($requeteUser);
+    echo "fin";
+
 }
 
  ?>
